7b380d4bf4f0f3a319bd6eb934b5b271951073d3
Pinak Jain <PJain@theladders.com>
2013-04-25 09:50:14 -0400

    Removing relationship between environment and dynamic environment
    Adding specific environment characteristics to dynamic environment

--- The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/DynamicEnvironment.java
+++ The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/DynamicEnvironment.java
@@ -2,6 +2,7 @@ package com.theladders.solid.lsp;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -12,23 +13,25 @@ import java.util.Set;
  * @author Zhi-Da Zhong &lt;zz@theladders.com&gt;
  */
 
-public class DynamicEnvironment extends Environment
+public class DynamicEnvironment
 {
   private final Environment         base;
+  private final HashMap<Object, Object> specificEnv;
   private final Map<String, String> keyMap; // map insecure prop names to secure ones
 
-  public DynamicEnvironment(Environment base, Map<String, String> propKeyMap)
+  public DynamicEnvironment(Environment base, Map<String, String> propKeyMap, HashMap<Object, Object> specificEnv)
   {
     this.base = base;
     this.keyMap = propKeyMap;
+    this.specificEnv = specificEnv;
   }
 
-  @Override
+ 
   public Collection<Object> values()
   {
     // TODO remove masked values
     // TODO join local instance values
-    return base.values();
+    return base.getValues();
   }
 
   /**
@@ -40,32 +43,33 @@ public class DynamicEnvironment extends Environment
    * @return The value for the given key after mapping (e.g. "home" might be mapped to "secureHome")
    */
 
-  @Override
   public Object get(Object key)
   {
     String realKey = keyMap.get(key);
-    Object value = super.get(realKey != null ? realKey : key);
+    Object value = specificEnv.get(realKey != null ? realKey : key);
     if (value == null)
     {
-      return base.get(realKey != null ? realKey : key);
+      return base.getEnvDataForKey(realKey != null ? realKey : key);
     }
     return value;
   }
 
-  @Override
   public Set<Map.Entry<Object, Object>> entrySet()
   {
-    Set<Map.Entry<Object, Object>> entrySet = new HashSet<>(super.entrySet());
-    entrySet.addAll(base.entrySet());
+    Set<Map.Entry<Object, Object>> entrySet = new HashSet<>(specificEnv.entrySet());
+    entrySet.addAll(base.getEntrySet());
     return Collections.unmodifiableSet(entrySet);
   }
 
-  @Override
   public Set<Object> keySet()
   {
-    Set<Object> keySet = new HashSet<>(super.keySet());
+    Set<Object> keySet = new HashSet<>(specificEnv.keySet());
     keySet.addAll(keyMap.keySet());
-    keySet.addAll(base.keySet());
+    keySet.addAll(base.getKeySet());
     return Collections.unmodifiableSet(keySet);
+  }
+  
+  public Map<String, String> getKeyMap(){
+	  return this.keyMap;
   }
 }
