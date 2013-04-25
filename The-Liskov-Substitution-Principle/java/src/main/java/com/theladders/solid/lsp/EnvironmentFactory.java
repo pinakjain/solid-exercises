270df6958a6c5315590dd61a86c90fa0ad643fd3
Pinak Jain <PJain@theladders.com>
2013-04-25 09:51:26 -0400

    Adding dynamic environment factory settings
    Later on will move them to its own factory

--- The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/EnvironmentFactory.java
+++ The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/EnvironmentFactory.java
@@ -1,17 +1,42 @@
 package com.theladders.solid.lsp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // Just a stub. Nothing to see here.
 
 public class EnvironmentFactory
 {
-  public static Environment getEnvironmentFor(String hostName)
-  {
-    Environment env = new Environment();
+	public static Environment getEnvironmentFor(String hostName)
+	{
+		HashMap<Object, Object> envMap = new HashMap<Object, Object>();
+		Environment env = new Environment(envMap);
 
-    env.put("isSSL", "true");
-    env.put("home", "http://" + hostName);
-    env.put("secureHome", "https://" + hostName);
+		env.putEnvData("isSSL", "true");
+		env.putEnvData("home", "http://" + hostName);
+		env.putEnvData("secureHome", "https://" + hostName);
 
-    return env;
-  }
+		return env;
+	}
+	
+	public static DynamicEnvironment getSecuredDynamicEnv(Environment baseEnv, Map<String, String> propKeyMap){
+		HashMap<Object, Object> dynEnvMap = new HashMap<Object, Object>();
+		DynamicEnvironment dynEnv = new DynamicEnvironment(baseEnv, propKeyMap, dynEnvMap);
+		
+		return dynEnv;
+	}
+	
+	public static DynamicEnvironment getInSecuredDynamicEnv(Environment baseEnv, Map<String, String> propKeyMap){
+		HashMap<Object, Object> dynEnvMap = new HashMap<Object, Object>();
+		DynamicEnvironment dynEnv = new DynamicEnvironment(baseEnv, propKeyMap, dynEnvMap);
+		
+		return dynEnv;
+	}
+	
+	public static DynamicEnvironment getNoSslDynamicEnv(Environment baseEnv, Map<String, String> propKeyMap){
+		HashMap<Object, Object> dynEnvMap = new HashMap<Object, Object>();
+		DynamicEnvironment dynEnv = new DynamicEnvironment(baseEnv, propKeyMap, dynEnvMap);
+		
+		return dynEnv;
+	}
 }
