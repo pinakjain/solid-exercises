b1b363977dd6d23f0c771a219f531b96664c0cfc
Pinak Jain <PJain@theladders.com>
2013-04-29 10:43:48 -0400

    Moving methods to corresponding interfaces

--- The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobCommon.java
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobCommon.java
@@ -10,75 +10,13 @@ import com.theladders.solid.isp.oldjob.stubs.Region;
 
 public interface JobCommon
 {
-  /**
-   * @return the name of the company
-   */
-  String getCompany();
-
-  /**
-   * Gets the value of the company_size_id field.
-   * This represents the id in the company size table for the description of
-   * how large the company is.
-   *
-   * @return companySize
-   */
-  Integer getCompanySize();
-
-  /**
-   * Get this job's compensation (text).
-   *
-   * @return compensation for this job.
-   */
-  String getCompensation();
-
-  /**
-   * Get this job's compensationSalary (text).
-   *
-   * @return compensationSalary for this job.
-   */
-  String getCompensationSalary();
-
-  /**
-   * Get this job's compensationBonus (text).
-   *
-   * @return compensationBonus for this job.
-   */
-  String getCompensationBonus();
-
-  /**
-   * Get this job's compensationOther (text).
-   *
-   * @return compensationOther for this job.
-   */
-  String getCompensationOther();
-
-  /**
-   * Retrieves a list of disciplines for this job.
-   *
-   * @return List of Disciplines
-   */
-  List<Discipline> getDisciplines();
-
+  
   /**
    * Get the date this job was entered into the system.
    *
    * @return the Date the job was entered.
    */
   Date getEntryDate();
-
-  /**
-   * Returns an object that represents the number of years of experience
-   * that are required for this job.
-   * @return experience object
-   */
-  Experience getExperience();
-
-  /**
-   * Get the Industry for this job.
-   *
-   * @return the Industry for this job.
-   */
-  Industry getIndustry();
 
   String getLocation();
 
@@ -107,12 +45,6 @@ public interface JobCommon
 
   int getSubscriberId();
 
- /**
-   * Get this job's title.
-   *
-   * @return the title for this job.
-   */
-  String getTitle();
 
   /**
    * @return returns true if this job was posted anonymously
@@ -137,10 +69,5 @@ public interface JobCommon
    */
   Date getUpdateTime();
 
-  /**
-   * Refactored so it can be used by both job and JobReq
-   * @return fullJobDescription()
-   *
-   */
-  String getDescription();
+  
 }
