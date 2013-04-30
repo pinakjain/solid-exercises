19fa12ebdb6214462bd5d7a51885ca2b8b988c85
Pinak Jain <PJain@theladders.com>
2013-04-30 17:37:31 -0400

    Refactoring the code for removing dependency

--- The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SubscriberArticleManagerImpl.java
+++ The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SubscriberArticleManagerImpl.java
@@ -1,27 +1,12 @@
 package com.theladders.solid.dip;
 
 import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class SubscriberArticleManagerImpl implements SubscriberArticleManager
 {
-  private static final String              IMAGE_PREFIX       = "http://somecdnprodiver.com/static/images/careerAdvice/";
-  private static final Map<String, String> CATEGORY_IMAGE_MAP = new HashMap<>();
-
-  static
-  {
-    CATEGORY_IMAGE_MAP.put("Interviewing", "interviewing_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Job Search", "job_search_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Networking", "networking_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Personal Branding", "personalBranding_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Resume", "resume_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Salary", "salary_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("Assessment", "salary_thumb.jpg");
-    CATEGORY_IMAGE_MAP.put("On the Job", "salary_thumb.jpg");
-  }
 
   private SuggestedArticleDao              suggestedArticleDao;
   private RepositoryManager                repositoryManager;
@@ -42,67 +27,30 @@ public class SubscriberArticleManagerImpl implements SubscriberArticleManager
             .andSuggestedArticleSourceIdEqualTo(1);
 
     criteria.setOrderByClause("create_time desc");
-    List<SuggestedArticle> dbSuggestions = this.suggestedArticleDao.selectByExampleWithBlobs(criteria);
+    List<SuggestedArticle> dbSuggestions = suggestedArticleDao.selectByExampleWithBlobs(criteria);
 
     // Fetch content associated with SuggestedArticle (based on externalArticleId)
-    resolveArticles(dbSuggestions);
+    SubscriberArticleProcessor articleProcessor = new SubscriberArticleProcessor(repositoryManager);
+    articleProcessor.resolveArticles(dbSuggestions);
 
     return dbSuggestions;
   }
 
   public int addSuggestedArticle(SuggestedArticle suggestedArticle)
   {
-    Integer STATUS_UNREAD = 1;
-    Integer HTP_CONSULTANT = 1;
-    suggestedArticle.setSuggestedArticleStatusId(STATUS_UNREAD);
-    suggestedArticle.setSuggestedArticleSourceId(HTP_CONSULTANT);
-    suggestedArticle.setCreateTime(new Date()); // current date
-    suggestedArticle.setUpdateTime(new Date()); // current date
-    int newId = suggestedArticleDao.insertReturnId(suggestedArticle);
-    return newId;
-  }
-
-  private void resolveArticles(List<SuggestedArticle> dbArticles)
-  {
-    for (SuggestedArticle article : dbArticles)
-    {
-
-      // Attempt to fetch the actual content;
-      ContentNode content = this.repositoryManager.getNodeByUuid(article.getArticleExternalIdentifier());
-      if (content != null && ContentUtils.isPublishedAndEnabled(content))
-      {
-        // Override miniImagePath
-        overrideMiniImagePath(content);
-        article.setContent(content);
-      }
-    }
-  }
-
-  private static void overrideMiniImagePath(ContentNode node)
-  {
-    String path = (String) node.getProperty("miniImagePath");
-
-    if (path == null || path.length() == 0)
-    {
-      String category = (String) node.getProperty("primaryTopic");
-      node.addProperty("miniImagePath", IMAGE_PREFIX + CATEGORY_IMAGE_MAP.get(category));
-    }
+    return suggestedArticleDao.addArticle(suggestedArticle);
   }
 
   public void updateNote(Integer id, String note)
   {
-    SuggestedArticle article = new SuggestedArticle();
-    article.setSuggestedArticleId(id);
+    SuggestedArticle article = suggestedArticleDao.getArticle(id);
     article.setNote(note);
-    suggestedArticleDao.updateByPrimaryKeySelective(article);
+    suggestedArticleDao.updateArticle(article);
   }
 
   public void markRecomDeleted(Integer id)
   {
-    Integer STATUS_DELETED = 4;
-    SuggestedArticle article = new SuggestedArticle();
-    article.setSuggestedArticleId(id);
-    article.setSuggestedArticleStatusId(STATUS_DELETED);
-    suggestedArticleDao.updateByPrimaryKeySelective(article);
+    suggestedArticleDao.delete(suggestedArticleDao.getArticle(id));
   }
+  
 }
--- The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SuggestedArticleDao.java
+++ The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SuggestedArticleDao.java
@@ -1,19 +1,40 @@
 package com.theladders.solid.dip;
 
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class SuggestedArticleDao
-{
-  public void updateByPrimaryKeySelective(@SuppressWarnings("unused") SuggestedArticle article) {}
+public class SuggestedArticleDao {
+	public void updateArticle(
+			@SuppressWarnings("unused") SuggestedArticle article) {
+	}
 
-  public int insertReturnId(@SuppressWarnings("unused") SuggestedArticle suggestedArticle)
-  {
-    return 0;
-  }
+	public int addArticle(
+			@SuppressWarnings("unused") SuggestedArticle suggestedArticle) {
+		// Integer STATUS_UNREAD = 1;
+		// Integer HTP_CONSULTANT = 1;
+		// suggestedArticle.setSuggestedArticleStatusId(STATUS_UNREAD);
+		// suggestedArticle.setSuggestedArticleSourceId(HTP_CONSULTANT);
+		// suggestedArticle.setCreateTime(new Date()); // current date
+		// suggestedArticle.setUpdateTime(new Date()); // current date
+		return 0;
+	}
 
-  public List<SuggestedArticle> selectByExampleWithBlobs(@SuppressWarnings("unused") SuggestedArticleExample criteria)
-  {
-    return Collections.singletonList(new SuggestedArticle());
-  }
+	public List<SuggestedArticle> selectByExampleWithBlobs(
+			@SuppressWarnings("unused") SuggestedArticleExample criteria) {
+		return Collections.singletonList(new SuggestedArticle());
+	}
+
+	public void delete(SuggestedArticle article) {
+		// Integer STATUS_DELETED = 4;
+		// SuggestedArticle article = new SuggestedArticle();
+		// article.setSuggestedArticleId(id);
+		// article.setSuggestedArticleStatusId(STATUS_DELETED);
+	}
+
+	public SuggestedArticle getArticle(Integer id) {
+		return new SuggestedArticle();
+	}
 }
